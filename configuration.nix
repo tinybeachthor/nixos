dc0c286d0fe95936c6d3422b27f3b6803022594f
Martin Toman <whomenope@outlook.com>
2020-05-06 00:08:28 +0200

    Add automated nix garbage collection

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,17 @@
       ./desktop.nix
     ];
 
+  # Nix
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+  nix.extraOptions = ''
+    min-free = ${toString (100 * 1024 * 1024)}
+    max-free = ${toString (1024 * 1024 * 1024)}
+  '';
+  nix.autoOptimiseStore = true;
 
   # Kernel
   boot.kernelPackages = pkgs.linuxPackages_hardened;
