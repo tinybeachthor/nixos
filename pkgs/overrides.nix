f19e12d4b129eac55cf7370ae9616daeee13f2d8
Martin Toman <whomenope@outlook.com>
2020-05-29 15:33:09 +0200

    Add default empty config for alacritty&neovim

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 
 {
-  alacritty = { config, alacritty ? pkgs.alacritty }:
+  alacritty = { config ? "", alacritty ? pkgs.alacritty }:
     pkgs.callPackage ./alacritty.nix {
       inherit alacritty;
       inherit config;
     };
-  neovim = { config, neovim ? pkgs.neovim }:
+  neovim = { config ? "", neovim ? pkgs.neovim }:
     pkgs.callPackage ./neovim.nix {
       inherit neovim;
       inherit config;
