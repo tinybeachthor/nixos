78c2d7f31f883b31b83a6daa8898e8f6fb615c5d
Martin Toman <toman.martin@live.com>
2020-09-03 20:24:37 +0200

    Add tinybeachthor cachix

---
+++ cachix/tinybeachthor.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://tinybeachthor.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "tinybeachthor.cachix.org-1:RZYoc0wjSysHAeZ1GxPGSfeLjnSxN4/B3lNL1uWPdeA="
+    ];
+  };
+}
+    
