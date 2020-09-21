4ebcbb574289d9839bef8b666e88ec081a698105
Martin Toman <toman.martin@live.com>
2020-09-21 15:10:26 +0200

    Remove smosModule

--- users.nix
+++ users.nix
@@ -1,12 +1,4 @@
 { pkgs, ... }:
-
-let
-  smosModule = (builtins.fetchGit {
-    url = "https://github.com/NorfairKing/smos";
-    ref = "master";
-    rev = "b3587542b07e682789f98367e7a7dc3ea57bd9bf";
-  } + "/nix/program.nix");
-in
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
@@ -32,20 +24,11 @@ in
   home-manager.users = {
     martin = { pkgs, ... }: {
       imports = [
-        smosModule
       ];
-
-      programs.smos = {
-        enable = true;
-        extraConfig = ''
-          workflow-dir: "/home/martin/.smos/workflow"
-        '';
-      };
 
       home.packages = with pkgs; [ ];
     };
   };
-
   home-manager.useGlobalPkgs = true;
   # home-manager.useUserPackages = true;
 }
