a7d33554efcd3cd9077aa0d5e0b6889e3e77fc7f
Martin Toman <toman.martin@live.com>
2020-10-04 21:48:10 +0200

    Remove xfce, cleanup modules/i3.nix

--- modules/i3.nix
+++ modules/i3.nix
@@ -17,34 +17,24 @@
     synaptics.enable = false;
 
     # Desktop
-    desktopManager = {
-      xfce.enable = true;
-      xterm.enable = false;
-    };
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
         rofi
         i3status-rust
         i3lock-color
-      ];
+
+        conky    # cpu stats
+        xss-lock # x sesssion locker
+        ];
       package = pkgs.i3-gaps;
-    };
-    displayManager = {
-      sddm.enable = true;
     };
     displayManager.defaultSession = "none+i3";
   };
 
-  services.upower.enable = true; # needed by i3status-rust for battery stats
+  services.upower.enable = true;  # needed by i3status-rust for battery stats
 
   environment.systemPackages = with pkgs; [
-      # i3status-rust utils
-      conky    # cpu stats
-
-      xss-lock # x sesssion locker
-
-      # x utils
       xclip
       xorg.xhost
   ];
