bec7cbc885e4db233a3c7cb6ee3f411e3baa3c17
Martin Toman <toman.martin@live.com>
2020-10-05 18:41:31 +0200

    Do not enable jackd service

--- modules/sound.nix
+++ modules/sound.nix
@@ -18,18 +18,12 @@
     };
   };
 
-  services.jack = {
-    jackd.enable = true;
-    # support ALSA only programs via ALSA JACK PCM plugin
-    alsa.enable = false;
-    # support ALSA only programs via loopback device (supports programs like Steam)
-    loopback = {
-      enable = true;
-      # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-      #dmixConfig = ''
-      #  period_size 2048
-      #'';
-    };
+  environment = {
+    systemPackages = with pkgs; [
+      jack2Full
+      jack_capture
+      qjackctl
+    ];
   };
 
   services.headphones.enable = true;
