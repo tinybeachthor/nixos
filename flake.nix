1a00f32e06e761691feb0e2695234ddf688d9e81
Martin Toman <toman.martin@live.com>
2020-10-19 01:20:23 +0200

    Add my-nur input & overlay nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,9 +2,24 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
     home-manager.url = github:rycee/home-manager/release-20.09;
+
+    my-nur.url = github:tinybeachthor/nur-packages/master;
+    my-nur.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations.ALBATROSS = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, my-nur }:
+  {
+    nixosConfigurations.ALBATROSS = nixpkgs.lib.nixosSystem rec {
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: with my-nur.packages.${system}; {
+             inherit vimPlugins;
+          })
+        ];
+        config = {
+          allowUnfree = true;
+        };
+      };
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
