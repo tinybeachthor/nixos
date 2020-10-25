c54645844333ca3d2568ee2dd2dc299c68c0065d
Martin Toman <toman.martin@live.com>
2020-10-25 21:07:15 +0100

    Update nixpkgs, tinybeachthor follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
     home-manager.url = github:rycee/home-manager/release-20.09;
-    tinybeachthor.url = github:tinybeachthor/nur-packages/master;
+    tinybeachthor = {
+      url = github:tinybeachthor/nur-packages/master;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { self, nixpkgs, home-manager, tinybeachthor }:
   {
